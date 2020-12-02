d64a72a1d5e0e8a49dbfbc0a40fa7d644faeb591
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2020-12-02 22:57:37 +1100

    added notion packages and custom page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '@styles/globals.css'
+import 'react-notion-x/src/styles.css' // core styles shared by all of react-notion-x (required)
+import 'prismjs/themes/prism-tomorrow.css' // used for code syntax highlighting (optional)
+import 'rc-dropdown/assets/index.css' // used for collection views (optional)
+import 'katex/dist/katex.min.css' // used for rendering equations (optional)
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
